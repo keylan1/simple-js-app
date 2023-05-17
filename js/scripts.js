6b34ef151baf0a728459b79e6cce76d5173e15d9
Sarah.Keller <sarah29.84@gmail.com>
2023-05-17 18:41:57 +0200

    Added pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
-alert("Hello world");
+let pokemonList = [
+  {name: 'Charizard',
+  height: 1.7,
+  type: ['fire', 'flying']
+  },
+  {name: 'Lucario',
+  height: 1.2,
+  type: ['steel', 'fighting']
+  },
+  {name: 'Altaria',
+  height: 1.1,
+  type: ['dragon', 'flying']
+  },
 
-let favoriteFood = "tomatoes";
-document.write(favoriteFood);
+];
