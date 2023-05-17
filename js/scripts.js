3adde95a2e1e7fc94ee865dd8faae463c924ada7
Sarah.Keller <sarah29.84@gmail.com>
2023-05-17 19:19:43 +0200

    Renamed key 'type' to 'types'

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ let pokemonList = [
   {
     name: 'Charizard',
     height: 1.7,
-    type: ['fire', 'flying']
+    types: ['fire', 'flying']
   },
   {
     name: 'Lucario',
     height: 1.2,
-    type: ['steel', 'fighting']
+    types: ['steel', 'fighting']
   },
   {
     name: 'Altaria',
     height: 1.1,
-    type: ['dragon', 'flying']
+    types: ['dragon', 'flying']
   },
 ];
