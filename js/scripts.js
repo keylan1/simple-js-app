067c11c808a97808cb3908ebcf044549911e7e2b
Sarah.Keller <sarah29.84@gmail.com>
2023-05-22 20:50:24 +0200

    Added functions add() and getAll() and return object for pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,38 @@
-let pokemonRepository = (function() {
-  
-  let pokemonList = [
-  {
-    name: 'Charizard',
-    height: 1.7,
-    types: ['fire', 'flying']
-  },
-  {
-    name: 'Lucario',
-    height: 1.2,
-    types: ['steel', 'fighting']
-  },
-  {
-    name: 'Altaria',
-    height: 1.1,
-    types: ['dragon', 'flying']
-  },
-];
 
-return {}
+let pokemonRepository = (function () {
+
+  let pokemonList = [
+    {
+      name: 'Charizard',
+      height: 1.7,
+      types: ['fire', 'flying']
+    },
+    {
+      name: 'Lucario',
+      height: 1.2,
+      types: ['steel', 'fighting']
+    },
+    {
+      name: 'Altaria',
+      height: 1.1,
+      types: ['dragon', 'flying']
+    },
+  ];
+
+  function add(item) {
+    return pokemonList.push(item); //function that adds item to pokemonList
+  }
+
+  function getAll() {
+    return pokemonList; //function that prints pokemonList
+  }
+
+  return { //return value of pokemonRepository
+    add: add,
+    getAll: getAll
+  }
 
 })();
-
-/* looping through pokemonList to print name and height to the DOM using template literals */
-// If pokemon.height is larger than 1.5, 'wow that's big' is added.
-
-/* for (let i = 0; pokemonList[i]; i++) {
-  if (pokemonList[i].height > 1.5) {
-    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that\'s big!</p>`);
-  } else {
-    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height})</p>`);
-  }
-} */
 
 /* Using a forEach() loop instead of for loop to iterate over every Pokémon in the array) */
 
@@ -42,14 +43,3 @@ pokemonList.forEach(pokemon => {
     document.write(`<p>${pokemon.name} (height: ${pokemon.height})</p>`);
   }
 });
-
-/*In the “scripts.js” file of your project, wrap your pokemonList array in an IIFE to avoid accidentally accessing the global state.
-Before anything else, create a new pokemonRepository variable to hold what your IIFE will return, then assign the IIFE to that variable.
-The IIFE should return an object with the following public functions assigned as keys:
-
-    getAll: return all items (pokemonRepository.getAll(); should return the pokemonList array)
-    add: add a single item to the pokemonList array (calling pokemonRepository.add(item); should add the Pokémon referred to with item to the pokemonList array)
-
-Make sure both functions are defined separately with the function keyword. Also, the IIFE returns only an object with the same names for keys as values (see the last paragraph of the Exercise)
-Outside of and below the IIFE, you should already have a forEach() loop that iterates over each Pokémon in the repository. But since you’ve limited access to the pokemonList array that’s inside the IIFE (so that it’s only accessible through one of the two functions returned by the IIFE), you need to update the loop code to cope with the new changes. Essentially, you need to use one of the two functions returned by the IIFE in order to retrieve the pokemonList array.
-*/
