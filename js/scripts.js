63de4f79cb1cc25d52ab537307252873a1dc8e39
Sarah.Keller <sarah29.84@gmail.com>
2023-06-02 18:29:00 +0200

    Offset to -400

--- js/scripts.js
+++ js/scripts.js
@@ -128,7 +128,7 @@ let pokemonRepository = (function () {
     });
 
     if (foundPokemon) {
-      const offsetTop = foundPokemon.offsetTop - 200;
+      const offsetTop = foundPokemon.offsetTop - 400;
       window.scrollTo({ top: offsetTop, behavior: "smooth" });
 
       // Add highlight to the found Pokemon button
